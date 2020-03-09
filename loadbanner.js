a9f2084aaa268f5737949fe0c23c1da1fb7768f8
Jackie Chui <jackiechui@Jackies-MacBook-Pro.local>
2020-03-08 17:43:26 -0700

    added a function that checks for window size retina, but not sure how to make canvas non retina from retina

--- loadbanner.js
+++ loadbanner.js
@@ -1,23 +1,36 @@
-// import html2canvas from "./node_modules/html2canvas/dist/html2canvas.js";
-// import { html2canvas } from './node_modules/html2canvas/dist/html2canvas.js';
-// import html2canvas from '.node_modules/html2canvas';
-
 const currentDirectory = document.getElementById("directory").innerHTML;
 
 var requireScript = document.createElement("script");
 requireScript.src = `${currentDirectory}\\node_modules\\html2canvas\\dist\\html2canvas.js`;
 document.body.appendChild(requireScript);
 
+
+
+// if(isRetinaDisplay()) {
+//   console.log('retina detected will divide picture size by half')
+//   screenshot.width = Math.floor(screenshot.width / 2);
+//   screenshot.height = Math.floor(screenshot.height / 2);
+// }
+
 setTimeout(() => {
+  const banner = document.getElementById("outer_wrapper");
   html2canvas(document.getElementById("outer_wrapper")).then(function(canvas) {
     const adNum = document.getElementById("adNum").innerHTML;
-    console.log("apprending canvas");
     canvas.id = `myScreenshot_${adNum}`;
     document.body.appendChild(canvas);
   });
 }, 2000);
 
 tl.timeScale(50);
+
+// this just checks if the whole WINDow of the app is in a retina dispaly, not the image
+// so this might need to be changed so that it checks the image instead of the window in the future
+// function isRetinaDisplay() {
+//   if (window.matchMedia) {
+//       var mq = window.matchMedia("only screen and (min--moz-device-pixel-ratio: 1.3), only screen and (-o-min-device-pixel-ratio: 2.6/2), only screen and (-webkit-min-device-pixel-ratio: 1.3), only screen  and (min-device-pixel-ratio: 1.3), only screen and (min-resolution: 1.3dppx)");
+//       return (mq && mq.matches || (window.devicePixelRatio > 1)); 
+//   }
+// }
 
 // requirejs(
 //   [`${currentDirectory}\\node_modules\\html2canvas\\dist\\html2canvas.js`],
