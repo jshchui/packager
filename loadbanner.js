4407f94987a91e4637fd7e15c2c0c772629a5ef7
Jackie Chui <jackiechui@Jackies-MacBook-Pro.local>
2020-03-08 19:10:45 -0700

    if its retina, it will now unretina the backup image

--- loadbanner.js
+++ loadbanner.js
@@ -4,17 +4,12 @@ var requireScript = document.createElement("script");
 requireScript.src = `${currentDirectory}\\node_modules\\html2canvas\\dist\\html2canvas.js`;
 document.body.appendChild(requireScript);
 
-
-
-// if(isRetinaDisplay()) {
-//   console.log('retina detected will divide picture size by half')
-//   screenshot.width = Math.floor(screenshot.width / 2);
-//   screenshot.height = Math.floor(screenshot.height / 2);
-// }
-
+const banner = document.getElementById("outer_wrapper");
+if (isRetinaDisplay()) {
+  banner.style.transform = "scale(0.5)"
+}
 setTimeout(() => {
-  const banner = document.getElementById("outer_wrapper");
-  html2canvas(document.getElementById("outer_wrapper")).then(function(canvas) {
+  html2canvas(banner).then(function(canvas) {
     const adNum = document.getElementById("adNum").innerHTML;
     canvas.id = `myScreenshot_${adNum}`;
     document.body.appendChild(canvas);
@@ -25,12 +20,12 @@ tl.timeScale(50);
 
 // this just checks if the whole WINDow of the app is in a retina dispaly, not the image
 // so this might need to be changed so that it checks the image instead of the window in the future
-// function isRetinaDisplay() {
-//   if (window.matchMedia) {
-//       var mq = window.matchMedia("only screen and (min--moz-device-pixel-ratio: 1.3), only screen and (-o-min-device-pixel-ratio: 2.6/2), only screen and (-webkit-min-device-pixel-ratio: 1.3), only screen  and (min-device-pixel-ratio: 1.3), only screen and (min-resolution: 1.3dppx)");
-//       return (mq && mq.matches || (window.devicePixelRatio > 1)); 
-//   }
-// }
+function isRetinaDisplay() {
+  if (window.matchMedia) {
+      var mq = window.matchMedia("only screen and (min--moz-device-pixel-ratio: 1.3), only screen and (-o-min-device-pixel-ratio: 2.6/2), only screen and (-webkit-min-device-pixel-ratio: 1.3), only screen  and (min-device-pixel-ratio: 1.3), only screen and (min-resolution: 1.3dppx)");
+      return (mq && mq.matches || (window.devicePixelRatio > 1)); 
+  }
+}
 
 // requirejs(
 //   [`${currentDirectory}\\node_modules\\html2canvas\\dist\\html2canvas.js`],
