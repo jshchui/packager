a014883afdd7ea98cc9c60f7364e0a508585f36f
Jackie Chui <jackiechui@Jackies-MacBook-Pro.local>
2020-03-22 15:36:55 -0700

    test

--- tests/minifyFiles.test.js
+++ tests/minifyFiles.test.js
@@ -1,6 +1,8 @@
 
 const fs = require("fs")
 const minifyFiles = require("../modules/minifyFiles.js")
+
+
 const testBannerDir = __dirname.replace("/tests", "");
 
 // test("looping through path", async () => {
